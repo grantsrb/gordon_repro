d58526d5b9954d09420bcdc92582d31b750f1e60
Saetch Mo <grantsrb@gmail.com>
2020-11-24 16:28:05 -0800

    add tentative model changes

--- locgame/models.py
+++ locgame/models.py
@@ -264,13 +264,19 @@ class SimpleCNN(CNNBase):
         self.shapes = []
         shape = self.img_shape[-2:]
         self.shapes.append(shape)
-        chans = [32,64,128,256,self.emb_size]
+        if self.img_shape[1] <= 84:
+            chans = [32,64,128,256,self.emb_size]
+            stride = 1
+            ksize = 3
+        else:
+            chans = [3,32,64,128,256,self.emb_size]
+            stride = 2
+            ksize = 5
+            print("using extra layer for larger image size")
         self.chans = chans
-        ksize = 3
-        stride = 1
         padding = 0
         block = self.get_conv_block(in_chan=self.img_shape[-3],
-                                    out_chan=32,
+                                    out_chan=self.chans[0],
                                     ksize=ksize,
                                     stride=stride,
                                     padding=padding,
